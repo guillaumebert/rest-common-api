c3ea21db913b940b02eb5ebfbcb50a7e4e98b6df
alexis nouvel <alexis.nouvel@neotys.com>
2019-09-11 16:43:06 +0200

    add QualityStatus

---
+++ data/QualityStatus.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Copyright (c) 2019, Neotys
+ * All rights reserved.
+ */
+namespace NeotysRestCommonAPI.Data
+{
+    /// <summary>
+	/// Quality Status
+    /// 
+    /// @author anouvel
+    /// 
+    /// </summary>
+    public enum QualityStatus
+    {
+        PASSED, FAILED, UNKNOWN
+    }
+}
